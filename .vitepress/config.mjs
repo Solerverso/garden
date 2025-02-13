17d7bc4b4a0e42fae3833e9c1baeb8424daf8aa7
Solerverso <123025364+Solerverso@users.noreply.github.com>
2025-02-13 19:02:28 -0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,78 @@ import { withSidebar } from "vitepress-sidebar";
 import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links';
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
 
+const vitePressConfigs = {
+logo: '/logo.svg',
+  locales: {
+    root: {
+      label: 'Português',
+      lang: 'pt'
+    },
+  },
+  
+  lang: 'pt',
+  vite: { 
+    optimizeDeps: { 
+      exclude: [ 
+        '@nolebase/vitepress-plugin-enhanced-readabilities/client', 
+        'vitepress', 
+        '@nolebase/ui', 
+      ], 
+    }, 
+    ssr: { 
+      noExternal: [ 
+        // If there are other packages that need to be processed by Vite, you can add them here. //
+        '@nolebase/vitepress-plugin-enhanced-readabilities', 
+        '@nolebase/ui', 
+      ], 
+    }, 
+  }, 
+  base: '/',
+  title: "Solerverso",
+  description: "Hello",
+ignoreDeadLinks: true,
+  themeConfig: {
+    search: {
+      provider: 'local'
+    },
+    docFooter: {
+      next: false,
+      prev: false,
+    },
+    
+  appearance: 'dark',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Sobre', link: '/sobre' },
+      { text: 'Teste', link: '/markdown-examples' }
+    ],
+
+    sidebar: generateSidebar(
+      {
+       documentRootPath: '/',
+        manualSortFileNameByPriority: ['sobre', 'notas', 'jornal', 'literatura'],
+      useFolderLinkFromSameNameSubFile: true,
+        collapseDepth: 1,
+        collapsed: true,
+        capitalizeFirst: true,
+        sortFolderTo: 'bottom',
+    }),
+    
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  },
+markdown: {
+    config: (md) => {
+      md.use(BiDirectionalLinks()) 
+    }
+}
+} satisfies UserConfig<DefaultTheme.Config>
+
+
+
 
 export default defineConfig(withSidebar(vitePressConfigs, [{
   documentRootPath: "/docs",

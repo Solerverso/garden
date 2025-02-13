0fe287f45c369e373839388eeb49b9bd5c471659
Solerverso <123025364+Solerverso@users.noreply.github.com>
2025-02-13 16:49:28 -0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'vitepress';
 import { generateSidebar } from 'vitepress-sidebar';
 import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links';
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
+import {VPBTheme} from "@chunge16/vitepress-blogs-theme";
+
 
 
 // https://vitepress.dev/reference/site-config
 
 export default defineConfig({
+      extends: VPBTheme,
+
 logo: '/logo.svg',
   locales: {
     root: {
@@ -44,6 +48,31 @@ ignoreDeadLinks: true,
       next: false,
       prev: false,
     },
+
+      blog: {
+          path: "/blog",
+          title: 'Blog',
+          description: 'All these articles were written by chunge!',
+          defaultAuthor: 'chunge',
+          categoryIcons: {
+              article: 'i-[carbon/notebook]', 
+            tutorial: 'i-[carbon/book]', 
+            document: 'i-[carbon/document]',
+          }, 
+          tagIcons: {
+              github: 'i-[carbon/logo-github]', 
+              vue: 'i-[logos/vue]',
+              javascript: 'i-[logos/javascript]',
+              'web development': 'i-[carbon/development]',
+              html: 'i-[logos/html-5]',
+              git: 'i-[logos/git-icon]',
+              vite: 'i-[logos/vitejs]',
+              locked: 'i-[twemoji/locked]',
+              react: 'i-[logos/react]',
+              blog: 'i-[carbon/blog]',
+              comment: 'i-[carbon/add-comment]',
+          },
+    
   appearance: 'dark',
     // https://vitepress.dev/reference/default-theme-config
     nav: [

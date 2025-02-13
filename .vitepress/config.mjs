9f976174d178f0e2e20652996617042123994771
Solerverso <123025364+Solerverso@users.noreply.github.com>
2025-02-13 19:00:18 -0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -5,7 +5,7 @@ import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links';
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
 
 
-export defineConfig(withSidebar(vitePressConfigs, [{
+export default defineConfig(withSidebar(vitePressConfigs, [{
   documentRootPath: "/docs",
   scanStartPath: "notes",
   resolvePath: "/notes/",
@@ -29,81 +29,3 @@ export defineConfig(withSidebar(vitePressConfigs, [{
   useFolderLinkFromIndexFile: true,
   useFolderTitleFromIndexFile: true,
 }]))
-
-// https://vitepress.dev/reference/site-config
-
-export default defineConfig({
-logo: '/logo.svg',
-  locales: {
-    root: {
-      label: 'Português',
-      lang: 'pt'
-    },
-  },
-  
-  lang: 'pt',
-  vite: { 
-    optimizeDeps: { 
-      exclude: [ 
-        '@nolebase/vitepress-plugin-enhanced-readabilities/client', 
-        'vitepress', 
-        '@nolebase/ui', 
-      ], 
-    }, 
-    ssr: { 
-      noExternal: [ 
-        // If there are other packages that need to be processed by Vite, you can add them here. //
-        '@nolebase/vitepress-plugin-enhanced-readabilities', 
-        '@nolebase/ui', 
-      ], 
-    }, 
-  }, 
-  base: '/',
-  title: "Solerverso",
-  description: "Hello",
-ignoreDeadLinks: true,
-  themeConfig: {
-    search: {
-      provider: 'local'
-    },
-    docFooter: {
-      next: false,
-      prev: false,
-    },
-    
-  appearance: 'dark',
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Sobre', link: '/sobre' },
-      { text: 'Teste', link: '/markdown-examples' }
-    ],
-
-    sidebar: generateSidebar(
-      {
-       documentRootPath: '/',
-        manualSortFileNameByPriority: ['sobre', 'notas', 'jornal', 'literatura'],
-      useFolderLinkFromSameNameSubFile: true,
-        collapseDepth: 1,
-        collapsed: true,
-        capitalizeFirst: true,
-        sortFolderTo: 'bottom',
-    }),
-    
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  },
-markdown: {
-    config: (md) => {
-      md.use(BiDirectionalLinks()) 
-    },
-  }})
-
-const vitePressSidebarOptions = {
-  // VitePress Sidebar's options here...
-  documentRootPath: '/',
-  collapsed: true,
-  capitalizeFirst: true
-};

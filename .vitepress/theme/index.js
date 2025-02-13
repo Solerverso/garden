9ad8f6c60eb0a1486b44be236abea3f879e65e97
Solerverso <123025364+Solerverso@users.noreply.github.com>
2025-02-12 22:26:03 -0300

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,10 @@
-// You can directly import Vue files in the theme entry
-// VitePress is pre-configured with @vitejs/plugin-vue.
-import Layout from './Layout.vue'
+// https://vitepress.dev/guide/custom-theme
+import Theme from "vitepress/theme";
+import "./style.css";
+import { Layout } from "./Layout";
 
 export default {
+  extends: Theme,
   Layout,
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-}
+  enhanceApp({ app, router, siteData }) {},
+};

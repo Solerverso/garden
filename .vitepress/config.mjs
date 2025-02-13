60bf51040a7964e28b49ad42779a51ddc2a42e2e
Solerverso <123025364+Solerverso@users.noreply.github.com>
2025-02-13 18:54:40 -0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitepress';
-import { withSidebar } from 'vitepress-sidebar';
+import { withSidebar } from "vitepress-sidebar",
 import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links';
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar'
 
@@ -12,6 +12,32 @@ const vitePressSidebarOptions = {
 // https://vitepress.dev/reference/site-config
 
 export default defineConfig({
+(withSidebar(vitePressConfigs, [{
+  documentRootPath: "/docs",
+  scanStartPath: "notes",
+  resolvePath: "/notes/",
+  useTitleFromFileHeading: true,
+  useFolderLinkFromIndexFile: true,
+  useFolderTitleFromIndexFile: true,
+}, {
+  documentRootPath: "/docs",
+  scanStartPath: "/en/notes",
+  resolvePath: "/en/notes/",
+  basePath: "/",
+  useTitleFromFileHeading: true,
+  useFolderLinkFromIndexFile: true,
+  useFolderTitleFromIndexFile: true,
+}, {
+  documentRootPath: "/docs",
+  scanStartPath: "/jp/notes",
+  resolvePath: "/jp/notes/",
+  basePath: "/",
+  useTitleFromFileHeading: true,
+  useFolderLinkFromIndexFile: true,
+  useFolderTitleFromIndexFile: true,
+}]))
+
+  
 logo: '/logo.svg',
   locales: {
     root: {

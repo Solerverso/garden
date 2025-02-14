3440c095febd4d7f88cb60cfdcd1f2401daf506e
Solerverso <123025364+Solerverso@users.noreply.github.com>
2025-02-14 02:23:23 -0300

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,16 +3,61 @@ import { defineConfig } from 'vitepress';
 import { withSidebar } from "vitepress-sidebar";
 import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links';
 import { calculateSidebar } from '@nolebase/vitepress-plugin-sidebar';
+
+import path from 'path';
+import { writeFileSync } from 'fs';
 import { Feed } from 'feed';
+import { defineConfig, createContentLoader, type SiteConfig } from 'vitepress';
 
 
-// This is the only directory we care about including in our RSS feed for now
-const blogDir = 'notas';
-const formattedPagesForRSS: Record<string, string> = {};
+const hostname: string = 'https://solerverso.github.io'
+
 
 const vitePressConfigs = {
 
+  buildEnd: async (config: SiteConfig) => {
+    const feed = new Feed({
+      title: 'Paul Laros',
+      description: 'My personal blog',
+      id: hostname,
+      link: hostname,
+      language: 'en',
+      image: 'https://laros.io/images/paul-laros.jpg',
+      favicon: `${hostname}/favicon.ico`,
+      copyright:
+        'Copyright (c) 2023-present, Paul Laros'
+    }),
 
+    const posts = await createContentLoader('*.md', {
+      excerpt: true,
+      render: true
+    }).load(),
+
+    posts.sort(
+      (a, b) =>
+        +new Date(b.frontmatter.date as string) -
+        +new Date(a.frontmatter.date as string)
+    ),
+
+    for (const { url, excerpt, frontmatter, html } of posts) {
+      feed.addItem({
+        title: frontmatter.title,
+        id: `${hostname}${url}`,
+        link: `${hostname}${url}`,
+        description: excerpt,
+        content: html,
+        author: [
+          {
+            name: 'Paul Laros',
+            email: 'hey@laros.io',
+            link: 'https://laros.io/authors/paul'
+          }
+        ],
+        date: frontmatter.date
+      })
+    },
+
+writeFileSync(path.join(config.outDir, 'feed.rss'), feed.rss2()),
   
 logo: '/logo.svg',
   locales: {
